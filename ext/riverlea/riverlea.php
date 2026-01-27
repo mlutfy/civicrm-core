6d5419f020416cb1487165186b9ca5c45c3cd304
benjamin <ben@civicrm.org>
2026-01-27 17:36:46 -0500

    Riverlea stream UI (listing, previewing, editing)

--- ext/riverlea/riverlea.php
+++ ext/riverlea/riverlea.php
@@ -33,3 +33,51 @@ function riverlea_civicrm_install() {
 function riverlea_civicrm_enable() {
   _riverlea_civix_civicrm_enable();
 }
+
+/**
+ * Implements search tasks hook to add the `activate` action
+ *
+ * @param array $tasks
+ * @param bool $checkPermissions
+ * @param int|null $userId
+ */
+function riverlea_civicrm_searchKitTasks(array &$tasks, bool $checkPermissions, ?int $userId) {
+  if ($checkPermissions && !CRM_Core_Permission::check('administer CiviCRM', $userId)) {
+    return;
+  }
+  $tasks['RiverleaStream']['activate_backend'] = [
+    'title' => E::ts('Activate for Backend'),
+    'icon' => 'fa-briefcase',
+    'number' => '=== 1',
+    'apiBatch' => [
+      'action' => 'activate',
+      'params' => ['backOrFront' => 'backend'],
+      'confirmMsg' => E::ts('Activate stream for backend pages?'),
+      'runMsg' => E::ts('Activating stream...'),
+      'successMsg' => E::ts('Stream activated. You may need to refresh the page or clear your browser cache to see the full effect.'),
+      'errorMsg' => E::ts('An error occurred while attempting to activate the stream.'),
+    ],
+  ];
+  $tasks['RiverleaStream']['activate_frontend'] = [
+    'title' => E::ts('Activate for Frontend'),
+    'icon' => 'fa-shop',
+    'number' => '=== 1',
+    'apiBatch' => [
+      'action' => 'activate',
+      'params' => ['backOrFront' => 'frontend'],
+      'confirmMsg' => E::ts('Activate stream for frontend pages?'),
+      'runMsg' => E::ts('Activating stream...'),
+      'successMsg' => E::ts('Stream activated. You may need to refresh the page or clear your browser cache to see the full effect.'),
+      'errorMsg' => E::ts('An error occurred while attempting to activate the stream.'),
+    ],
+  ];
+  $tasks['RiverleaStream']['preview'] = [
+    'title' => E::ts('Preview'),
+    'icon' => 'fa-eye',
+    'number' => '=== 1',
+    'crmPopup' => [
+      'path' => "/civicrm",
+      'data' => "{name: name.join(',')",
+    ],
+  ];
+}
